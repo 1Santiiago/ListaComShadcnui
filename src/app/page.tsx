b0b24636b1fd9b41db3c2bba0592e29151626355
Santiago Ferreira <santiago@orientme.com.br>
2025-04-04 13:26:43 -0300

    Lista de compras usando shadcnui, nextjs e react-hook-form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,138 @@
-import Image from "next/image";
+"use client";
+import { Button } from "@/components/ui/button";
+import { useForm, SubmitHandler } from "react-hook-form";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+
+import { Delete, FilePenLine, Pen, PlusCircle } from "lucide-react";
+import { useState } from "react";
+import { ModeToggle } from "@/components/mode-toggle";
+
+interface ProdutoPros {
+  id: number;
+  name: string;
+  quantidade: number;
+}
+
+type Inputs = {
+  name: string;
+  price: number;
+  quantidade: number;
+};
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const { register, handleSubmit, reset } = useForm<Inputs>();
+  const [Produto, SetProdutos] = useState<ProdutoPros[]>([]);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+  const onSubmit: SubmitHandler<Inputs> = (data) => {
+    const newProduct: ProdutoPros = {
+      id: Produto.length + 1,
+      name: data.name,
+      quantidade: data.quantidade,
+    };
+    SetProdutos([...Produto, newProduct]);
+    reset();
+  };
+
+  const handleDelete = (id: number) => {
+    const updatedProdutos = Produto.filter((produto) => produto.id !== id);
+    SetProdutos(updatedProdutos);
+  };
+
+  return (
+    <div className="mx-auto p-6 max-w-2xl space-y-2 ">
+      <div className="flex justify-end">
+        <ModeToggle />
+      </div>
+      <div className="flex justify-between">
+        <h1 className="text-3xl">Listas de Compras</h1>
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button>
+              <PlusCircle />
+              Adicionar Produtos
+            </Button>
+          </DialogTrigger>
+          <DialogContent>
+            <DialogHeader>
+              <DialogTitle>Novo Produto</DialogTitle>
+            </DialogHeader>
+
+            <form className="space-y-3" onSubmit={handleSubmit(onSubmit)}>
+              <div className="flex gap-4">
+                <Label htmlFor="name">Nome:</Label>
+                <Input
+                  type="text"
+                  placeholder="Nome do Produto"
+                  id="name"
+                  {...register("name")}
+                />
+              </div>
+              <div className="flex gap-4">
+                <Label htmlFor="price">Quantidade:</Label>
+                <Input
+                  type="text"
+                  placeholder="Quantidade de produtos"
+                  id="price"
+                  {...register("quantidade", { valueAsNumber: true })}
+                />
+              </div>
+              <DialogFooter>
+                <DialogClose asChild>
+                  <Button type="submit" variant={"link"}>
+                    Cancelar
+                  </Button>
+                </DialogClose>
+                <DialogClose asChild>
+                  <Button type="submit">Enviar</Button>
+                </DialogClose>
+              </DialogFooter>
+            </form>
+          </DialogContent>
+        </Dialog>
+      </div>
+      <div className="border rounded-2xl">
+        <Table>
+          <TableHeader>
+            <TableHead>Ordem Lista</TableHead>
+            <TableHead>Nome</TableHead>
+            <TableHead className="text-center">Quantidades</TableHead>
+          </TableHeader>
+          <TableBody>
+            {Produto.map((e) => (
+              <TableRow key={e.id}>
+                <TableCell>{e.id}</TableCell>
+                <TableCell>{e.name}</TableCell>
+                <TableCell className="text-center">{e.quantidade}</TableCell>
+                <div className="flex my-3 gap-2 justify-center items-center">
+                  <Delete
+                    size={16}
+                    color="red"
+                    onClick={() => handleDelete(e.id)}
+                  />
+                </div>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </div>
     </div>
   );
 }
